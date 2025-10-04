f5d5da4eb6f5429d87b0899f962bfc3d49cdf990
Sesaretah <theshafiei@icloud.com>
2025-10-04 13:22:41 +0200

    1

--- app/models/shortner.rb
+++ app/models/shortner.rb
@@ -1,4 +1,6 @@
 class Shortner < ApplicationRecord
   has_many :items
   belongs_to :user, optional: true
+
+  validates :url, presence: true, uniqueness: { message: "has already been shortened" }
 end
