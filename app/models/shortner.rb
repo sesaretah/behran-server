5e88e2c1d2577806b5d726fb426640879676a63f
Sesaretah <theshafiei@icloud.com>
2025-10-04 13:03:03 +0200

    1

--- app/models/shortner.rb
+++ app/models/shortner.rb
@@ -1,12 +1,4 @@
 class Shortner < ApplicationRecord
   has_many :items
   belongs_to :user, optional: true
-  
-  validates :url, presence: true, format: { 
-    with: URI::regexp(%w[http https]), 
-    message: "must be a valid URL" 
-  }
-  validates :url, uniqueness: { 
-    message: "has already been shortened" 
-  }
 end
